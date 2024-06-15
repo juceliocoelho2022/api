a467d6a73742642f48614a03ed907d91bd332290
JUCELIO <juceliocoelho2022@gmail.com>
2024-06-15 16:11:36 -0300

    fat: add Controller

--- api/src/main/java/com/eventostec/api/service/AddressService.java
+++ api/src/main/java/com/eventostec/api/service/AddressService.java
@@ -7,8 +7,6 @@ import com.eventostec.api.repositories.AddressRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
-
 @Service
 public class AddressService {
 
